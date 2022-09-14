1fa185698fb02a748bbfa6d09a9268667563b334
MatheusMunizera <matheus.munizera@gmail.com>
2022-09-14 20:42:33 -0300

    ajuste serviço indisponivel

--- src/app/services/count.service.ts
+++ src/app/services/count.service.ts
@@ -1,40 +1,45 @@
-import { Injectable } from '@angular/core';
-import { Photo } from '@capacitor/camera';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
-import { PhotoService } from './photo.service';
-import { Router } from '@angular/router';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
+import { ToastController } from "@ionic/angular";
+
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CountService {
-
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router, private toastController: ToastController) {}
 
   private readonly COUNT_API_URL = "http://localhost:8080/test";
 
   public async sendBinaryImage(photoArray: Uint8Array | String) {
-
-    var toApiObject = { "image": photoArray, "other_key": "value" };
-
-    return await new Promise(resolve => {
-      try {
-        this.http.post<string>(this.COUNT_API_URL, toApiObject)
-          .subscribe(data => {
-
-            if (data)
-              resolve(JSON.parse(JSON.stringify(data)))
-
-          })
-      } catch (ex) {
-        console.log(ex, "Error.CountService.SendBinaryImage")
-      }
-    });
+    var toApiObject = { image: photoArray, other_key: "value" };
+    
+    this.router.navigate([`/`]);
+    this.presentToast();
+    
+    try {
+      return await new Promise((resolve, reject) => {
+        this.http
+          .post<string>(this.COUNT_API_URL, toApiObject)
+          .subscribe((data) => {
+            resolve(JSON.parse(JSON.stringify(data)));
+          });
+      });
+    } catch (error) {
+        console.log('Erro CountService.SendBindaryImage')
+    }
+    
   }
 
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Serviço de contagem indisponivel no momento',
+      duration: 3000,
+      position: 'bottom',
+      color: 'danger',
+      cssClass: 'text-black'
+    });
+    await toast.present();
+  }
 }
-
-
-
